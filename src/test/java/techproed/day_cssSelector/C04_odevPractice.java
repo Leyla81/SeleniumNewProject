96865edd4588e5c2636e105cf5a94273663c7f18
Leyla <leylacayli@hotmail.com>
2023-06-04 23:09:50 +0300

    02.06.2023

---
+++ src/test/java/techproed/day_cssSelector/C04_odevPractice.java
@@ -0,0 +1,46 @@
+package techproed.day_cssSelector;
+
+public class C04_odevPractice {
+    public static void main(String[] args) {
+
+       /*
+        ÖDEV
+         // http://the-internet.herokuapp.com/add_remove_elements/ adresine gidiniz
+         // Add Element butonuna 100 defa basınız
+         // 100 defa basıldığını test ediniz
+         // 90 defa delete butonuna basınız
+         // 90 defa basıldığını doğrulayınız
+         // Sayfayı kapatınız
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
